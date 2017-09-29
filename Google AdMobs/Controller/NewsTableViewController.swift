f42241921c3a03015fc683cb3ff17d9198fd9e0b
devxris <chuwei0915@gmail.com>
2017-09-29 12:15:43 +0800

    implement slide-down effect after ad loaded to adBannerView

--- Google AdMobs/Controller/NewsTableViewController.swift
+++ Google AdMobs/Controller/NewsTableViewController.swift
@@ -65,8 +65,15 @@ extension NewsTableViewController: GADBannerViewDelegate {
 	
 	func adViewDidReceiveAd(_ bannerView: GADBannerView) {
 		print("Banner loaded successfully")
-		tableView.tableHeaderView?.frame = bannerView.frame
-		tableView.tableHeaderView = bannerView
+		
+		// Reposition the banner ad to create a slide down effect
+		let translateTransform = CGAffineTransform(translationX: 0, y: -bannerView.bounds.size.height)
+		bannerView.transform = translateTransform
+		UIView.animate(withDuration: 0.5) {
+			self.tableView.tableHeaderView?.frame = bannerView.frame
+			bannerView.transform = CGAffineTransform.identity
+			self.tableView.tableHeaderView = bannerView
+		}
 	}
 	
 	func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
